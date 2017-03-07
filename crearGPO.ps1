8e6893f4ac30bf0ae5b42be28df30e1bd7de6422
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2017-03-07 19:26:02 +0100

    Update crearGPO.ps1

--- crearGPO.ps1
+++ crearGPO.ps1
@@ -9,10 +9,10 @@ New-ADGroup -Name "Alumnado" -SamAccountName Alumnado -GroupCategory Security -G
 
 #Creamos los usuarios y los asiganmos en el grupo correspondiente
 
-New-ADUser -Name Rafa -GivenName Rafa -Surname Aybar -Path "CN=Alumnado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
-New-ADUser -Name Al1 -GivenName Al1 -Surname 2 -Path "CN=Alumnado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
-New-ADUser -Name Al2-GivenName Al2-Surname 2 -Path "CN=Alumnado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
-New-ADUser -Name Prof1 -GivenName 1 -Surname 1 -Path "CN=Profesorado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
-New-ADUser -Name Prof2 -GivenName 2 -Surname 2 -Path "CN=Profesorado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
+New-ADUser -Name Rafa -GivenName Rafa -Surname Aybar -Path "OU=Alumnado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
+New-ADUser -Name Al1 -GivenName Al1 -Surname 2 -Path "OU=Alumnado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
+New-ADUser -Name Al2-GivenName Al2-Surname 2 -Path "OU=Alumnado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
+New-ADUser -Name Prof1 -GivenName 1 -Surname 1 -Path "OU=Profesorado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
+New-ADUser -Name Prof2 -GivenName 2 -Surname 2 -Path "OU=Profesorado,DC=rafael,DC=org" -accountPassword (ConvertTo-SecureString -AsPlainText "Rafa-1994" -Force)
 Add-ADGroupMember "Alumnado" Rafa,Al1,Al2
 Add-ADGroupMember "Profesorado" Prof1,Prof2
